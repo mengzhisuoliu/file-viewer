5309dd94e9c374b8826e08c27edbeb3a9b5ee6c6
小爬的老粉丝 <727842003@qq.com>
2026-05-19 08:44:57 +0800

    Refresh 1.0.6 viewer artifacts

--- dist/src/package/vendors/word/docx.d.ts
+++ dist/src/package/vendors/word/docx.d.ts
@@ -1,4 +1,5 @@
+import { AppWrapper } from '../../common/type';
 /**
  * 渲染docx文件
  */
-export default function (buffer: ArrayBuffer, target: HTMLDivElement): Promise<void>;
+export default function (buffer: ArrayBuffer, target: HTMLDivElement): Promise<AppWrapper>;
